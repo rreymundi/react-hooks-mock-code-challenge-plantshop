4368601c32553c5ed3398886f7f49a52088b51a1
Raúl Reymundi <raul@kickstarter.com>
2022-08-11 17:43:44 -0400

    Done.

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const { id, name, image, price } = plant
+  const [inStock, setStock] = useState(true)
+
+  function handleStockToggle(){
+    setStock((inStock) => !inStock)
+  }
+
   return (
-    <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+    <li key={id} className="card">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      <button className="primary" onClick={handleStockToggle}>{inStock ? "In Stock" : "Out of Stock"}</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data))
+  }, [])
+
+  function handleAddPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  const displayedPlants = plants.filter((plant) => 
+     plant.name.toLowerCase().includes(search.toLocaleLowerCase())
+     )
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={displayedPlants}/>
     </main>
   );
 }
